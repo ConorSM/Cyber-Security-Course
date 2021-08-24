bcc32f5bb9776866866fa8b2e783006a5dab0772
ConorSM <89384242+ConorSM@users.noreply.github.com>
2021-08-24 17:28:40 +0100

    Add files via upload

---
+++ Multiops.ps1
@@ -0,0 +1,28 @@
+$FIRST_NUM = Read-Host "Enter first number"
+$SECOND_NUM = Read-Host "Enter second number"
+
+$SUM = [int]$FIRST_NUM + [int]$SECOND_NUM
+$SUB = [int]$FIST_NUM - [int]$SECOND_NUM
+$MUL = [int]$FIRST_NUM * [int]$SECOND_NUM
+$DIV = try {[int]$FIRST_NUM / [int]$SECOND_NUM} catch {Write-Output "Can't divide by 0!"} 
+	
+Write-Host "$FIRST_NUM + $SECOND_NUM = $SUM"
+Write-Host "$FIRST_NUM - $SECOND_NUM = $SUB"
+Write-Host "$FIRST_NUM * $SECOND_NUM = $MUL"
+Write-Host "$FIRST_NUM / $SECOND_NUM = $DIV"
+
+
+Add-Content results.txt "$FIRST_NUM + $SECOND_NUM = $SUM"
+Add-Content results.txt "$FIRST_NUM - $SECOND_NUM = $SUB"
+Add-Content results.txt "$FIRST_NUM * $SECOND_NUM = $MUL"
+Add-Content results.txt "$FIRST_NUM / $SECOND_NUM = $DIV"
+
+
+
+
+
+
+
+
+
+
